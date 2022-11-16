cb61d87b5d8531b69c6f966b545c7d65b77f6e97
mohsen1080 <myoosefz@uoguelph.ca>
2022-11-16 17:09:39 -0500

    Change the Logo

--- R/Information.R
+++ R/Information.R
@@ -11,7 +11,7 @@ information_ui <- shiny::column(
     shiny::h4(shiny::strong('Version 1.0.5')),
     shiny::hr(),
     shiny::tags$blockquote(
-      shiny::img(src = 'www/PictureInt.png', align = "center", width = "100%", height = "100%")
+      shiny::img(src = 'www/AllInOne3.gif', align = "right", width = "40%", height = "40%")
     ),
     shiny::hr(),
     #br(),
